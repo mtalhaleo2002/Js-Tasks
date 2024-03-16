c44a0b51fb4deec1eccfb9631023eb0c2b3fa710
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-16 09:18:11 +0500

    Variables For Number

---
+++ Chater 3/src.js
@@ -0,0 +1,22 @@
+// Task 1 
+
+var caseQty;
+console.log(caseQty);
+
+// Task 2
+
+caseQty = 144;
+console.log(caseQty);
+
+// Task 3
+
+// var num = "3"; //This statement cannot used in Math operation
+var num = 9;
+console.log(num);
+
+// Task 4
+
+var number;
+number = 38 + 64;
+console.log(number)
+ 
