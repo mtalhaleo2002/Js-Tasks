186f9373cfac6343112b4e24170ccddc6332f9d6
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-16 09:04:31 +0500

    Alert

---
+++ Chapter 1 Alerts/src.js
@@ -0,0 +1,15 @@
+// Task 1 
+
+alert("First Name");
+alert("Last Name");
+alert("Email");
+alert("Phone Number")
+alert("Password")
+
+// Task 2
+
+alert("You're learning JavaScript")
+
+// Task 3
+
+alert("Jiiiii Haaaaaan , Mein hun Khal Nayak")
