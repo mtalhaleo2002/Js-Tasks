c515d008598ebb20fb03b13be3ffb93a1a438459
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-16 09:12:15 +0500

    Variables For String

---
+++ Chapter 2/src.js
@@ -0,0 +1,23 @@
+// Task 1 
+
+var butterFly;
+console.log(butterFly)
+
+// Task 2
+
+var nickName;
+nickName = "Raju";
+console.log(nickName);
+
+// Task 3
+
+var teamName = "Super Elevens";
+alert(teamName);
+
+// Task 4
+
+var bestMan = "Charlie";
+console.log(bestMan);
+
+bestMan = "Jonathan";
+console.log(bestMan);
