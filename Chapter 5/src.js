29a463a822359c5d58595b00c64bafb01cacd68a
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-18 03:24:17 +0500

    Math Expression I

---
+++ Chapter 5/src.js
@@ -0,0 +1,30 @@
+// Task 1
+
+// modulus %
+
+// Task 2
+
+var num = 20 % 6;
+// Answer is 2
+console.log(num);
+
+//Task 3
+
+var largeNum = 1000 * 2000;
+console.log(largeNum);
+
+//Task 4
+
+num = largeNum - num;
+console.log(num);
+
+//Task 5
+
+var num1 = 23;
+var num2 =  6;
+var result = num1 % num2;
+console.log(result)
+
+// Task 6
+
+alert(5*18);
