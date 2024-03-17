e6898b98da9e37b46ef2d87a4dfe14473e1892d8
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-17 23:49:45 +0500

    Variables For Number

--- Chapter 3/src.js
+++ Chapter 3/src.js
@@ -1,4 +1,4 @@
-// Task 1 
+// Task 1
 
 var caseQty;
 console.log(caseQty);
@@ -18,11 +18,18 @@ console.log(num);
 
 var number;
 number = 38 + 64;
-console.log(number)
- 
-// Task 5 
+console.log(number);
+
+// Task 5
 
 var merchTotal = 100;
 var shippingCharge = 10;
 var orderTotal = merchTotal + shippingCharge;
-console.log(orderTotal)
+console.log(orderTotal);
+
+// Task 6
+
+var digit = 90;
+console.log(digit);
+digit = digit + 86;
+console.log(digit);
