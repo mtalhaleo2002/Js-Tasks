9a5bfad3e72b891d47b41589c15b0e3cf0e7cbe2
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-17 23:59:08 +0500

    Variable Names Legal And Illegal

---
+++ Chapter 4/src.js
@@ -0,0 +1,23 @@
+// Task 1
+
+var productCost = 3.45;
+console.log(productCost);
+
+// Task 2
+
+var nameOfBand;
+
+// Task 3
+
+var value_of_$ = 275.83;
+console.log(value_of_$);
+
+// Task 4
+var mohammadTalhaKhurshid;
+
+// Task 5
+
+var 86; // Variable can,t be a number 
+var Hello World; // Variable can,t contain spaces.
+var any_Text$; // Variable can be any text including dollar $ and _ underscore
+var ***&&; // Variable can,t be a special keyword
