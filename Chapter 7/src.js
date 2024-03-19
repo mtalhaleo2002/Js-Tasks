862c85535e885091274ed3ca1fc1df6058b74c08
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-19 05:29:20 +0500

    Math Expression III

---
+++ Chapter 7/src.js
@@ -0,0 +1,34 @@
+// Task 1 
+
+//calculatedNum = 14;
+
+// Task 2
+
+//calculatedNum = 24;
+
+// Task 3
+
+//calculatedNum = 24;
+
+// Task 4
+
+//calculatedNum = 18;
+
+
+// Task 5
+
+var cost;
+cost = (2 + 2) * (4 + 10);
+console.log(cost);
+
+// Task 6
+
+var units;
+units = 2 + (2 * 4) + 10;
+console.log(units);
+
+// Task 7
+
+var pressure;
+pressure = 4 / (2 * 4);
+console.log(pressure);
