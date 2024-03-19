473b9c2cb261b5ed6692ec972caeeb5a81b99b10
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-19 09:42:39 +0500

    if Statement Nested

---
+++ Chapter 14/src.js
@@ -0,0 +1,38 @@
+// Task 1
+
+var password = "Heig";
+if (password !== "") {
+    if (password.length <= 5) {
+        alert("Password must be greater than 5");
+    } else {
+        alert("OK");
+    }
+}
+
+// Task 2
+
+var a = 1;
+var c = "Max"
+if(a === 1){
+    if (c === "Max")
+    alert("OK");
+}
+
+// Task 3
+
+if(a === 1 || c === "Max"){
+    alert("OK");
+}
+
+// Task 4
+
+var value1 = 68;
+var value2 = 68;
+if(value1 === value2){
+    if(value1 <= value2){
+        alert("Hurray!");
+    }
+    else{
+        alert("Not Found");
+    }
+}
