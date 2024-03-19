83cf525ccdc3dedc2c232ca04d0e957e3c2a029b
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-19 05:21:16 +0500

    Math Expression II

---
+++ Chapter 6/src.js
@@ -0,0 +1,43 @@
+// Task 1
+
+var z = 3;
+console.log(z);
+z = z + 1;
+console.log(z);
+ z+=1; // Incrementing the value of variable x by one.
+ console.log(z);
+
+// Task 2
+
+var x = 100;
+console.log(x);
+x--;
+console.log(x);
+
+// Task 3
+
+var x = 50;
+var y = x++;
+// y value is 50
+console.log("Value of y: " + y);
+
+// Task 4
+
+var y = 50;
+var z = --y;
+//Now z becomes 49
+console.log("Value of z: " + z);
+
+// Task 5
+
+// newNum = num--;
+
+// Task 6 
+
+// newNumber = num + 1;
+
+// Task 7
+
+var digit = 78;
+digit++;
+alert(digit);
