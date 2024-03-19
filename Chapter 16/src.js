cad776fd9fd39909b066b101dc8cb4a9615b6208
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-19 10:01:25 +0500

    Array II

---
+++ Chapter 16/src.js
@@ -0,0 +1,24 @@
+// Task 1
+
+var elements = ["String"];
+console.log(elements);
+elements.push("Another String");
+console.log(elements);
+var lastElement = elements[elements.length - 1];
+console.log(lastElement);
+alert(lastElement);
+
+// Task 2
+
+var Alphabet=["h","i","j","k"];
+console.log(Alphabet);
+Alphabet.pop();
+console.log(Alphabet);
+
+
+// Task 3
+
+var Alphabet=["h","i","j","k"];
+console.log(Alphabet);
+Alphabet.push(87);
+console.log(Alphabet);
