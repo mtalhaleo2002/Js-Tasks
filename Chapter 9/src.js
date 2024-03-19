1d0b6a2d6b255ff9b3a0087c0fcd4aeedbed1d68
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-19 05:52:06 +0500

    Prompts

---
+++ Chapter 9/src.js
@@ -0,0 +1,28 @@
+// Task 1
+
+var firstName = prompt("Enter first name");
+console.log(firstName);
+
+// Task 2
+
+var country = prompt("Country?", "China" );
+console.log(country);
+
+// Task 3
+
+var yourName = prompt("Enter Your Name");
+
+// Task 4
+
+var message = prompt('"What,s Your message"','"Message should be in strings"');
+
+// Task 5
+
+var assign1 = "string1";
+var assign2 = "string2";
+var result = prompt(assign1, assign2);
+
+// Task 6
+
+var anything = prompt("What,s Your Favourite Poet?", "Mirza Ghalib");
+alert(anything);
