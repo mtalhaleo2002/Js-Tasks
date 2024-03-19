517b8aef7e9f27a05179be9ed29f0e5977178a5b
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-19 05:36:47 +0500

    Concatenating Text Strings

---
+++ Chapter 8/src.js
@@ -0,0 +1,34 @@
+// Task 1 
+
+// num = "4";
+
+// Task 2
+
+// message = "Hello,Dolly";
+var message;
+message = ("Hello," + "Dolly");
+alert(message);
+
+// Task 3
+
+// 333
+
+// Task 4
+
+alert("Pakistan " + "Zindabad")
+
+// Task 5
+
+var statement;
+statement = "string" + 78;
+console.log(statement);
+
+// Task 6
+
+var unit1;
+var unit2;
+unit1 = "string1";
+unit2 = "string2";
+
+var result = unit1 + unit2;
+console.log(result);
