3b2cc24a788ea7a12fec5b049e491d193a5432d9
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-19 06:04:01 +0500

    if statments

---
+++ Chapter 10/src.js
@@ -0,0 +1,33 @@
+// Task 1
+
+var city = "Karachi";
+if(city == "Karachi"){
+    console.log("The City Of Lights")
+}
+
+// Task 2
+
+var x = "9";
+var y = "9";
+if(x === y){
+    var another = prompt("Give me value of z");
+}
+
+// Task 3
+
+ZipCode = "10010";
+if(ZipCode === "10010"){
+    alert("Karachi");
+}
+else{
+    alert("Please write correct city");
+}
+
+// Task 4
+
+var number = 78;
+console.log(number);
+if(number === 78){
+    number = 165;
+}
+console.log(number);
