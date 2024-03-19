d716d96ffa018aed195ccd2599100fc4c488a874
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-19 09:52:04 +0500

    Array I

---
+++ Chapter 15/src.js
@@ -0,0 +1,23 @@
+// Task 1
+
+var array = [];
+console.log(array);
+
+// Task 2
+
+var oneString = ["String"];
+console.log(oneString);
+
+// Task 3
+
+var alphabet = ["h", "i", "j", "k", "l", "m", "n", "o"];
+var totalLength = alphabet.length;
+console.log("Total length of the array alphabet is: " + totalLength);
+
+// Task 4
+
+var elements = ["One Element"];
+console.log(elements);
+elements[1] = "Another Element";
+console.log(elements);
+alert(elements[1]);
