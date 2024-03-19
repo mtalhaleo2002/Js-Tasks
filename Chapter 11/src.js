54fb914f2ad99981eefc40a94ac41babe59d5598
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-19 06:14:21 +0500

    Comparison Operators

---
+++ Chapter 11/src.js
@@ -0,0 +1,40 @@
+// Task 1
+
+var number = 67;
+var another = 87;
+if (number != another){
+    number = another;
+}
+console.log(number, another)
+
+// Task 2
+
+value = 78;
+another = 79;
+if(value >= another){
+    value = 100;
+}
+console.log(value);
+
+
+// Task 3
+
+something = 78;
+console.log(something);
+if(something != 67){
+    something = 67;
+}
+console.log(something);
+
+// Task 4
+
+if(67 != 78){
+    alert("Congratulations");
+}
+
+// Task 5
+
+var x = prompt("Enter Your First Name");
+if(x != "John Wick"){
+    alert("No match");
+}
