e3cc95b2a373765ebb66c88da48d7ac944f5a8a1
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-30 06:59:45 +0500

    Rounding Numbers)

---
+++ Chapter 21/src.js
@@ -0,0 +1,26 @@
+// Task 1
+
+console.log(Math.round(4.998))
+
+// Task 2
+
+var origNum = 65.667;
+var roundNum = Math.round(origNum);
+console.log(roundNum);
+
+// Task 3
+
+var origNum = 178.974;
+var roundNum = Math.floor(origNum);
+console.log(roundNum);
+
+// Task 4
+
+var number = 78.888;
+var result = Math.round(number);
+console.log(result);
+
+// Task 5
+
+var x = Math.floor(.5);
+console.log(x);
