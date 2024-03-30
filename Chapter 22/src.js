9b98338fcea07bb6b23d79e73161262eb5ad211d
Mohammad Talha <m.talhaleo2002@gmail.com>
2024-03-30 07:02:31 +0500

    Random Numbers

---
+++ Chapter 22/src.js
@@ -0,0 +1,22 @@
+// Task 1
+
+var random = Math.random();
+var  number = Math.floor(random * 50) + 1;
+console.log(number);
+
+// Task 2
+
+var no = Math.random();
+console.log(no)
+
+// Task 3
+
+var unknown = Math.random()
+var dice = Math.floor(unknown * 6) + 1;
+console.log(dice);
+
+// Task 4
+
+var digit = Math.random();
+var toss = (digit < 0.5) ? "Head" : "Tail";
+console.log(toss);
